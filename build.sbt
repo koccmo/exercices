9eb194e1d2b13a14fddeb46b598dcb8bb3cf01f7
romans.vavilovs <koccmo@gmail.com>
2024-09-10 16:34:52 +0300

    ok

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "exercices"
+
+version := "1.0"
+
+scalaVersion := "2.13.10"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.2.19" % Test,
+  "org.scalatestplus" %% "scalacheck-1-15" % "3.2.11.0" % Test,
+)
---
+++ src/main/scala/com/evolution/bootcamp/basic/DataStructures.scala
@@ -0,0 +1,15 @@
+package com.evolution.bootcamp.basic
+
+object DataStructures {
+
+  // Exercise. Write a function that checks if all values in a `List` are equal.
+  // Think about what you think your function should return if `list` is empty, and why.
+  def allEqual[T](list: List[T]): Boolean = list.toSet.size <= 1
+
+  // Exercise. Calculate the total cost of all vegetables, taking vegetable amounts (in units) from
+  // `vegetableAmounts` and prices per unit from `vegetablePrices`. Assume the price is 10 if not available
+  // in `vegetablePrices`.
+  val totalVegetableCost: Int = {
+    17 // implement here
+  }
+}
